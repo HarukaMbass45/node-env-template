7d1948f3e25fb6eb32d61a8ebd547e7b34209462
KentaNakanishi <kenta_nakanishi@rebuild-inc.co.jp>
2023-04-06 19:07:11 +0900

    ts-node でconsoleを使えるようにした

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,26 +1,5 @@
-import express from 'express';
-import type { Request, Response, NextFunction } from 'express';
-import { sampleRouter } from '@/src/infrastructure/router/sample-router';
+const hello = (message: string): void => {
+  console.log('message is ' + message);
+};
 
-const app: express.Express = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// CORS対応
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  next();
-});
-
-app.get('/', (req: Request, res: Response) => {
-  res.status(200).send('Hello Express!');
-});
-
-app.use('/sample', sampleRouter());
-
-app.listen(3000, () => {
-  console.log('API Sever is running...');
-});
+hello('hoge');
